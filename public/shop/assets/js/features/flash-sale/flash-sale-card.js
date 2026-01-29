2ca5deec535f61b95899f75382f7da6dd44aa552
Yen Dev <yendev96@gmail.com>
2026-01-29 09:17:41 +0700

    OK

--- public/shop/assets/js/features/flash-sale/flash-sale-card.js
+++ public/shop/assets/js/features/flash-sale/flash-sale-card.js
@@ -38,7 +38,9 @@ export function createFlashSaleCard(product) {
     html += '<div class="bundle-discount-badge">-' + discount + '%</div>';
     html += '</div>';
     
-    // Button actions - 2 buttons like normal product card
+    html += '</div>'; // Close product-info
+    
+    // Button actions - OUTSIDE product-info for mobile grid layout
     html += '<div class="bundle-product-actions">';
     html += '<button class="bundle-btn-primary" onclick="window.flashSaleActions.buyNow(' + product.id + ', ' + product.flash_price + ')">';
     html += '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" style="width: 1.125rem; height: 1.125rem; display: inline-block; vertical-align: middle;"><path fill-rule="evenodd" d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 0 0 4.25 22.5h15.5a1.875 1.875 0 0 0 1.865-2.071l-1.263-12a1.875 1.875 0 0 0-1.865-1.679H16.5V6a4.5 4.5 0 1 0-9 0ZM12 3a3 3 0 0 0-3 3v.75h6V6a3 3 0 0 0-3-3Zm-3 8.25a3 3 0 1 0 6 0v-.75a.75.75 0 0 1 1.5 0v.75a4.5 4.5 0 1 1-9 0v-.75a.75.75 0 0 1 1.5 0v.75Z" clip-rule="evenodd" /></svg>';
@@ -48,8 +50,6 @@ export function createFlashSaleCard(product) {
     html += '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" style="width: 1.25rem; height: 1.25rem; display: inline-block;"><path fill-rule="evenodd" d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 0 0 4.25 22.5h15.5a1.875 1.875 0 0 0 1.865-2.071l-1.263-12a1.875 1.875 0 0 0-1.865-1.679H16.5V6a4.5 4.5 0 1 0-9 0ZM12 3a3 3 0 0 0-3 3v.75h6V6a3 3 0 0 0-3-3Zm-3 8.25a3 3 0 1 0 6 0v-.75a.75.75 0 0 1 1.5 0v.75a4.5 4.5 0 1 1-9 0v-.75a.75.75 0 0 1 1.5 0v.75Z" clip-rule="evenodd" /></svg>';
     html += '</button>';
     html += '</div>';
-    
-    html += '</div>'; // Close product-info
     html += '</div>'; // Close product-card
     
     return html;
