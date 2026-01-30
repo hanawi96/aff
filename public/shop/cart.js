2e0e506d3efc63938b4bad156b5c4a4e62410789
Yen Dev <yendev96@gmail.com>
2026-01-30 08:52:42 +0700

    KO

--- public/shop/cart.js
+++ public/shop/cart.js
@@ -1024,7 +1024,7 @@ const cart = {
         if (discountRow && discountCodeLabel && discountAmountElement) {
             if (discountAmount > 0) {
                 discountRow.classList.remove('hidden');
-                discountCodeLabel.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width: 0.875rem; height: 0.875rem; display: inline-block; vertical-align: middle; margin-right: 0.25rem; color: #10b981;"><path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" /></svg>(' + state.discount.code + ')';
+                discountCodeLabel.textContent = '(' + state.discount.code + ')';
                 discountAmountElement.textContent = '-' + utils.formatPrice(discountAmount);
             } else {
                 discountRow.classList.add('hidden');
