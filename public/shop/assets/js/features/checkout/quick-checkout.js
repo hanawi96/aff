ab64f9fbc9574d0f91bb5c1d61ebfe94d1a2fa13
Yen Dev <yendev96@gmail.com>
2026-01-31 18:34:02 +0700

    OK

--- public/shop/assets/js/features/checkout/quick-checkout.js
+++ public/shop/assets/js/features/checkout/quick-checkout.js
@@ -1372,8 +1372,19 @@ export class QuickCheckout {
         }
         
         const orderTotalDetail = document.getElementById('orderTotalDetail');
+        const orderTotalLabelText = document.getElementById('orderTotalLabelText');
+        
         if (orderTotalDetail) {
             orderTotalDetail.textContent = formatPrice(total);
+        }
+        
+        // Update total label based on payment method
+        if (orderTotalLabelText) {
+            if (this.paymentMethod === 'bank' && this.bankTransferConfirmed) {
+                orderTotalLabelText.textContent = 'Đã thanh toán:';
+            } else {
+                orderTotalLabelText.textContent = 'Tổng cộng:';
+            }
         }
         
         // Update payment row based on payment method
@@ -1382,30 +1393,12 @@ export class QuickCheckout {
         const orderPaymentAmount = document.getElementById('orderPaymentAmount');
         
         if (orderPaymentRow && orderPaymentLabel && orderPaymentAmount) {
-            if (this.paymentMethod === 'bank' && this.bankTransferConfirmed) {
-                // Đã chuyển khoản - hiển thị rõ ràng đã thanh toán
-                orderPaymentRow.classList.remove('hidden');
-                orderPaymentRow.classList.add('paid');
-                orderPaymentRow.classList.add('payment-note-simple');
-                orderPaymentRow.classList.remove('payment-row');
-                orderPaymentLabel.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" style="width: 1rem; height: 1rem; flex-shrink: 0;"><path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" /></svg><span style="white-space: nowrap;">Đã thanh toán ' + '</span><span style="white-space: nowrap;"> - Không cần trả thêm</span>';
-                orderPaymentAmount.textContent = '';
-            } else if (this.paymentMethod === 'bank' && !this.bankTransferConfirmed) {
-                // Chưa xác nhận chuyển khoản
-                orderPaymentRow.classList.remove('hidden');
-                orderPaymentRow.classList.remove('paid');
-                orderPaymentRow.classList.remove('payment-note-simple');
-                orderPaymentRow.classList.add('payment-row');
-                orderPaymentLabel.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" style="width: 1rem; height: 1rem; display: inline-block; vertical-align: middle;"><path d="M11.584 2.376a.75.75 0 0 1 .832 0l9 6a.75.75 0 1 1-.832 1.248L12 3.901 3.416 9.624a.75.75 0 0 1-.832-1.248l9-6Z" /><path fill-rule="evenodd" d="M20.25 10.332v9.918H21a.75.75 0 0 1 0 1.5H3a.75.75 0 0 1 0-1.5h.75v-9.918a.75.75 0 0 1 .634-.74A49.109 49.109 0 0 1 12 9c2.59 0 5.134.202 7.616.592a.75.75 0 0 1 .634.74Zm-7.5 2.418a.75.75 0 0 0-1.5 0v6.75a.75.75 0 0 0 1.5 0v-6.75Zm3-.75a.75.75 0 0 1 .75.75v6.75a.75.75 0 0 1-1.5 0v-6.75a.75.75 0 0 1 .75-.75ZM9 12.75a.75.75 0 0 0-1.5 0v6.75a.75.75 0 0 0 1.5 0v-6.75Z" clip-rule="evenodd" /><path d="M12 7.875a1.125 1.125 0 1 0 0-2.25 1.125 1.125 0 0 0 0 2.25Z" /></svg> Cần thanh toán trước';
-                orderPaymentAmount.textContent = formatPrice(total);
+            if (this.paymentMethod === 'bank') {
+                // Bank transfer - Hide payment row (not needed)
+                orderPaymentRow.classList.add('hidden');
             } else {
-                // COD
-                orderPaymentRow.classList.remove('hidden');
-                orderPaymentRow.classList.remove('paid');
-                orderPaymentRow.classList.remove('payment-note-simple');
-                orderPaymentRow.classList.add('payment-row');
-                orderPaymentLabel.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" style="width: 1rem; height: 1rem; display: inline-block; vertical-align: middle;"><path d="M10.464 8.746c.227-.18.497-.311.786-.394v2.795a2.252 2.252 0 0 1-.786-.393c-.394-.313-.546-.681-.546-1.004 0-.323.152-.691.546-1.004ZM12.75 15.662v-2.824c.347.085.664.228.921.421.427.32.579.686.579.991 0 .305-.152.671-.579.991a2.534 2.534 0 0 1-.921.42Z" /><path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v.816a3.836 3.836 0 0 0-1.72.756c-.712.566-1.112 1.35-1.112 2.178 0 .829.4 1.612 1.113 2.178.502.4 1.102.647 1.719.756v2.978a2.536 2.536 0 0 1-.921-.421l-.879-.66a.75.75 0 0 0-.9 1.2l.879.66c.533.4 1.169.645 1.821.75V18a.75.75 0 0 0 1.5 0v-.81a4.124 4.124 0 0 0 1.821-.749c.745-.559 1.179-1.344 1.179-2.191 0-.847-.434-1.632-1.179-2.191a4.122 4.122 0 0 0-1.821-.75V8.354c.29.082.559.213.786.393l.415.33a.75.75 0 0 0 .933-1.175l-.415-.33a3.836 3.836 0 0 0-1.719-.755V6Z" clip-rule="evenodd" /></svg> Thanh toán khi nhận hàng';
-                orderPaymentAmount.textContent = formatPrice(total);
+                // COD - Hide payment row (not needed, redundant with total)
+                orderPaymentRow.classList.add('hidden');
             }
         }
         
@@ -1437,14 +1430,19 @@ export class QuickCheckout {
         if (inlineTotal && totalLabelMain && savingsLabel && totalBox) {
             // Determine display based on payment method
             if (this.paymentMethod === 'bank' && this.bankTransferConfirmed) {
-                // Đã chuyển khoản - hiển thị 0đ
-                totalLabelMain.textContent = 'Thanh toán khi nhận';
-                inlineTotal.textContent = '0đ';
+                // Đã chuyển khoản - hiển thị "Đã thanh toán"
+                totalLabelMain.textContent = 'Đã thanh toán';
+                inlineTotal.textContent = formatPrice(total);
                 totalBox.classList.add('paid');
                 
-                // Show total order value in savings label
-                savingsLabel.textContent = 'Đơn hàng: ' + formatPrice(total);
-                savingsLabel.style.color = '#666';
+                // Show savings if any
+                const savings = this.discountAmount + (hasFreeShipping ? this.shippingFee : 0);
+                if (savings > 0) {
+                    savingsLabel.textContent = 'Tiết kiệm ' + formatPrice(savings);
+                    savingsLabel.style.color = '#27ae60';
+                } else {
+                    savingsLabel.textContent = '';
+                }
             } else if (this.paymentMethod === 'bank' && !this.bankTransferConfirmed) {
                 // Chưa xác nhận chuyển khoản
                 totalLabelMain.textContent = 'Cần thanh toán';
