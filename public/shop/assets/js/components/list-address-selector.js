0f3d4ffdd2c1f34839652ab22cbb76a9db125df7
Yen Dev <yendev96@gmail.com>
2026-01-31 17:54:22 +0700

    OK

--- public/shop/assets/js/components/list-address-selector.js
+++ public/shop/assets/js/components/list-address-selector.js
@@ -201,6 +201,9 @@ export class ListAddressSelector {
         
         listEl.innerHTML = filtered.map(item => `
             <div class="address-item" data-code="${item.code}">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="address-item-icon">
+                    <path fill-rule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clip-rule="evenodd" />
+                </svg>
                 <span class="item-name">${this.escapeHtml(item.name)}</span>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="chevron-icon">
                     <path fill-rule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clip-rule="evenodd" />
